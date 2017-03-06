b34dfd862163be8ea2521ad55433f31fa9ada194
TrenTech <trentech@gmail.com>
2017-03-06 09:07:59 -0500

    Add ItemSerializer

---
+++ src/main/java/com/gmail/trentech/pjc/core/ItemSerializer.java
@@ -0,0 +1,47 @@
+package com.gmail.trentech.pjc.core;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.data.DataView;
+import org.spongepowered.api.data.persistence.DataTranslators;
+import org.spongepowered.api.item.inventory.ItemStack;
+
+import ninja.leaping.configurate.ConfigurationNode;
+import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
+
+public class ItemSerializer {
+
+	public static String serialize(ItemStack itemStack) {
+		ConfigurationNode node = DataTranslators.CONFIGURATION_NODE.translate(itemStack.toContainer());
+
+		StringWriter stringWriter = new StringWriter();
+		
+		try {
+			HoconConfigurationLoader.builder().setSink(() -> new BufferedWriter(stringWriter)).build().save(node);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return stringWriter.toString();
+	}
+
+	public static ItemStack deserialize(String item) {
+		ConfigurationNode node = null;
+		
+		try {
+			node = HoconConfigurationLoader.builder().setSource(() -> new BufferedReader(new StringReader(item))).build().load();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		DataView dataView = DataTranslators.CONFIGURATION_NODE.translate(node);
+
+		return Sponge.getDataManager().deserialize(ItemStack.class, dataView).get();
+	}
+
+}
