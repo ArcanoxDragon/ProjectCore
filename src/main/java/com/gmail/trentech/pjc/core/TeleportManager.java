84da65eb0f3cb31f8f470f4daa2711dfb0cb6b8d
TrenTech <trentech@gmail.com>
2017-04-03 12:48:19 -0400

    Update TeleportManager

--- src/main/java/com/gmail/trentech/pjc/core/TeleportManager.java
+++ src/main/java/com/gmail/trentech/pjc/core/TeleportManager.java
@@ -1,5 +1,6 @@
 package com.gmail.trentech.pjc.core;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Consumer;
@@ -14,8 +15,21 @@ import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.TeleportHelper;
 import org.spongepowered.api.world.World;
 
+import com.flowpowered.math.vector.Vector3d;
+import com.google.common.collect.ImmutableList;
+
 public class TeleportManager {
 
+    private static final List<BlockType> unsafeBlocks = ImmutableList.of(
+            BlockTypes.AIR,
+            BlockTypes.CACTUS,
+            BlockTypes.FIRE,
+            BlockTypes.LAVA,
+            BlockTypes.FLOWING_LAVA,
+            BlockTypes.FLOWING_WATER,
+            BlockTypes.WATER
+        );
+    
 	private static ThreadLocalRandom random = ThreadLocalRandom.current();
 
 	public static Optional<Location<World>> getSafeLocation(Location<World> location) {
@@ -30,8 +44,12 @@ public class TeleportManager {
 			}
 			Location<World> unsafeLocation = optionalLocation.get();
 
+			if(!isInWorldBorder(unsafeLocation)) {
+				continue;
+			}
+			
 			BlockType blockType = unsafeLocation.getBlockType();
-
+			
 			if (!blockType.equals(BlockTypes.AIR) || !unsafeLocation.getRelative(Direction.UP).getBlockType().equals(BlockTypes.AIR)) {
 				continue;
 			}
@@ -41,9 +59,13 @@ public class TeleportManager {
 			for(int i2 = 0; i2 < 3; i2++) {
 				BlockType floorBlockType = floorLocation.getBlockType();
 				
-				if (floorBlockType.equals(BlockTypes.WATER) || floorBlockType.equals(BlockTypes.LAVA) || floorBlockType.equals(BlockTypes.FLOWING_WATER) || floorBlockType.equals(BlockTypes.FLOWING_LAVA) || floorBlockType.equals(BlockTypes.FIRE)) {
-					continue first;
-				}
+		        if (unsafeBlocks.contains(floorBlockType)) {
+		        	continue first;
+		        }
+				
+//				if (floorBlockType.equals(BlockTypes.WATER) || floorBlockType.equals(BlockTypes.LAVA) || floorBlockType.equals(BlockTypes.FLOWING_WATER) || floorBlockType.equals(BlockTypes.FLOWING_LAVA) || floorBlockType.equals(BlockTypes.FIRE)) {
+//					continue first;
+//				}
 				floorLocation = floorLocation.getRelative(Direction.DOWN);
 			}
 
@@ -76,6 +98,15 @@ public class TeleportManager {
 		return Optional.empty();
 	}
 
+    public static boolean isInWorldBorder(Location<World> location) {
+        World world = location.getExtent();
+
+        long radius = (long) (world.getWorldBorder().getDiameter() / 2.0);
+        Vector3d displacement = location.getPosition().sub(world.getWorldBorder().getCenter()).abs();
+
+        return !(displacement.getX() > radius || displacement.getZ() > radius);
+    }
+    
 	public static Consumer<CommandSource> setUnsafeLocation(Location<World> location) {
 		return (CommandSource src) -> {
 			Player player = (Player) src;
