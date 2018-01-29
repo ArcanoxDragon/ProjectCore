e56fe62772dfc8de4ae612dd413a06a911e613f2
TrenTech <TrenTech@gmail.com>
2018-01-29 10:17:05 -0500

    ItemSeralizer uses DataFormats

--- src/main/java/com/gmail/trentech/pjc/core/ItemSerializer.java
+++ src/main/java/com/gmail/trentech/pjc/core/ItemSerializer.java
@@ -1,36 +1,29 @@
 package com.gmail.trentech.pjc.core;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.StringReader;
-import java.io.StringWriter;
+import java.io.IOException;
 import java.util.Optional;
 
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.data.DataContainer;
+import org.spongepowered.api.data.persistence.DataFormats;
 import org.spongepowered.api.item.inventory.ItemStack;
-
-import com.google.common.reflect.TypeToken;
-
-import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
 
 public class ItemSerializer {
 
 	public static Optional<String> serialize(ItemStack item) {
 		try {
-			StringWriter sink = new StringWriter();
-			HoconConfigurationLoader loader = HoconConfigurationLoader.builder().setSink(() -> new BufferedWriter(sink)).build();
-			loader.save(loader.createEmptyNode().setValue(TypeToken.of(ItemStack.class), item));
-
-			return Optional.of(sink.toString());
-		} catch (Exception e) {
+			return Optional.of(DataFormats.JSON.write(item.toContainer()));
+		} catch (IOException e) {
+			e.printStackTrace();
 			return Optional.empty();
 		}
 	}
 
-	public static Optional<ItemStack> deserialize(String item) {
+	public static Optional<ItemStack> deserialize(String item) {	
 		try {
-			HoconConfigurationLoader loader = HoconConfigurationLoader.builder().setSource(() -> new BufferedReader(new StringReader(item))).build();
-
-			return Optional.of(loader.load().getValue(TypeToken.of(ItemStack.class)));
+			DataContainer container = DataFormats.JSON.read(item);
+			
+			return Optional.of(Sponge.getDataManager().deserialize(ItemStack.class, container).get());
 		} catch (Exception e) {
 			e.printStackTrace();
 			return Optional.empty();
